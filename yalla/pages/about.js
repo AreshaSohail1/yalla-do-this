873b0f4103273c87f00acadb9b9184614b9adcff
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-08-04 00:44:09 -0700

    saqib

---
+++ yalla/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "@/Components/organisms/Footer";
+import Headermain from "@/Components/organisms/Headermain";
+
+function about() {
+    return ( <
+        div >
+        <
+        Headermain / >
+        <
+        Footer / >
+        <
+        /div>
+    );
+}
+
+export default about;
