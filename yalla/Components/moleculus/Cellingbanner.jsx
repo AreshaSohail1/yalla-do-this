348492f39850a6d21b421d2f76d0c7a9fea56bb5
tkdking00 <musama4288921@gmail.com>
2024-08-05 03:40:07 -0700

    Responsive Complete

--- yalla/Components/moleculus/Cellingbanner.jsx
+++ yalla/Components/moleculus/Cellingbanner.jsx
@@ -13,7 +13,7 @@ const Cellingbanner = ({ content, button, paragraph }) => {
       setTimeout(() => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
         setFade(true);
-      },200)
+      })
 
 
 
@@ -24,10 +24,10 @@ const Cellingbanner = ({ content, button, paragraph }) => {
         style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
       >
         {/* <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
-        <div className="relative w-[50%] z-10 text-white pt-[10%] px-[8%] p-8">
-          <div className="text-[#1e73be] uppercase text-[50px] font-bold mb-5">{content}</div>
-          <div className="text-[18px] font-[300] leading-[30px]    ">{paragraph}</div>
-          <div className="mt-[6%] bg-[#164b8d] text-[24px]  cursor-pointer hover:bg-[#F97300] text-center ease-in duration-300 font-[300] rounded-[30px] py-[10px] px-[25px]">{button}</div>
+        <div className="relative flex flex-col  justify-center xl:items-start lg:items-start md:items-start sm:items-center items-center   z-10 text-white pt-[10%] px-[8%] p-8 xl:w-[50%] xl:text-start lg:w-[55%] lg:text-start md:w-[57%] md:mt-8 md:text-start sm:w-[100%] sm:mt-[150px] sm:text-center w-[100%] mt-[160px] text-center">
+          <div className="text-[#1e73be] uppercase  font-bold mb-5 xl:text-[50px] lg:text-[50px] md:text-[47px] sm:text-[45px] text-[40px]">{content}</div>
+          <div className=" font-[300]   xl:text-[18px] xl:leading-[30px] lg:text-[18px] lg:leading-[30px] md:text-[14px] md:leading-[20px] sm:text-[14px] sm:leading-[25px] text-[15px] leading-[20px]  ">{paragraph}</div>
+          <div className="mt-[6%] bg-[#164b8d]   cursor-pointer hover:bg-[#F97300] text-center ease-in duration-300 font-[300] rounded-[30px] py-[10px] px-[25px] xl:text-[24px] xl: w-[43%] lg:text-[20px] lg: w-[43%] md:text-[20px] md:w-[63%] sm:text-[17px] sm:w-[53%] text-[15px]  w-[53%]">{button}</div>
         </div>
       </div>
     </div>
--- yalla/Components/moleculus/Howwork.jsx
+++ yalla/Components/moleculus/Howwork.jsx
@@ -13,16 +13,16 @@ function Howwork() {
                 <Paragraph variant='offer'>We are a leading AC Repairing services in UAE. We provide the best service for your home and office appliance at an affordable price. With years of experience, we can handle any repair job large or small.</Paragraph>
             </div>
             <div className='pt-[4%]'>
-                <div className='flex gap-[50px]'>
-                    <div className=''>
+                <div className=' gap-[50px] xl:grid xl:grid-cols-3 lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 sm:grid sm:grid-cols-1 grid grid-cols-1'>
+                    <div className='xl:border-l-0 xl:border-b-0 lg:border-l-0 lg:border-b-0 md:border-l-0 md:border-b-2 sm:border-l-0 sm:border-b-2 sm:border-l-0 border-b-2 border-l-0'>
                     <Span level='work'>01.</Span>
                     <Span level='work2'>Communicate</Span>
                     </div>
-                    <div className='border-l-2 px-[2%] '>
+                    <div className=' px-[2%] xl:border-l-2 xl:border-b-0 lg:border-l-2 lg:border-b-0 md:border-l-0 md:border-b-2 sm:border-l-0 sm:border-b-2 sm:border-l-0 border-b-2 border-l-0 '>
                     <Span level='work'>02.</Span>
                     <Span level='work2'>Plans & Calculations</Span>
                     </div>
-                    <div className='border-l-2 px-[2%]  '>
+                    <div className=' px-[2%] xl:border-l-2 xl:border-b-0 lg:border-l-2 lg:border-b-0 md:border-l-0 md:border-b-2 sm:border-l-0 sm:border-b-2 sm:border-l-0 border-b-2 border-l-0  '>
                     <Span level='work'>03.</Span>
                     <Span level='work2'>Executions</Span>
                     </div>
