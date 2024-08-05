4e9d824b98295d57b5aec578a4dbb14e4819ee74
tkdking00 <musama4288921@gmail.com>
2024-08-05 03:47:52 -0700

    hmm

--- yalla/pages/service.js
+++ yalla/pages/service.js
@@ -4,11 +4,7 @@ import ServicesAll from '@/Components/organisms/ServicesAll'
 import Footer from '@/Components/organisms/Footer'
 import React from 'react'
 
-<<<<<<<< HEAD:yalla/pages/services.js
 function Services() {
-========
-function service() {
->>>>>>>> 4e357340fa0d9ed58c473475c3ecb6506d2279f0:yalla/pages/service.js
   return (
     <div>
       <ServicesAll />
@@ -21,8 +17,5 @@ function service() {
   )
 }
 
-<<<<<<<< HEAD:yalla/pages/services.js
 export default Services
-========
-export default service
->>>>>>>> 4e357340fa0d9ed58c473475c3ecb6506d2279f0:yalla/pages/service.js
+
