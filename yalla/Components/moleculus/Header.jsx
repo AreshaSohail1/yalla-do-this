76a75df23186bbefb365c512fb5ba219e5844444
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-08-05 02:50:22 -0700

    OKK

--- yalla/Components/moleculus/Header.jsx
+++ yalla/Components/moleculus/Header.jsx
@@ -52,13 +52,13 @@ function Header() {
             </AnchorTag>
             <ul className='dropdown  absolute bg-white opacity-5 z-[9999] invisible py-[10px] pr-[50px] pl-[30px]'>
               <li><AnchorTag variant='Headerh' href='/acservice'> AC Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href=''>over Servcies</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/service'>over Servcies</AnchorTag></li>
               <li><AnchorTag variant='Headerh' href='/tileservice'>Tiles Fixing</AnchorTag></li>
               <li><AnchorTag variant='Headerh' href='/electricservice'>Electrician Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/handyman'>Handyman & Plumber</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href=''>Electrician Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href=''>Cleaning Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href=''>Interior Design</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/plumbing'>Handyman & Plumber</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='bathroom'>Bathroom Services</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/painting'>Painting Services</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/kitchen'>Kitchen Service</AnchorTag></li>
               <li><AnchorTag variant='Headerh' href='/celling'>Gypsum Ceiling</AnchorTag></li>
             </ul>
           </li>
