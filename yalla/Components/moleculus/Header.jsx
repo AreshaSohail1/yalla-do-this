b69411166503a37dbe93f94ac9d28112035ee8c4
tkdking00 <musama4288921@gmail.com>
2024-08-06 05:00:36 -0700

    usama

--- yalla/Components/moleculus/Header.jsx
+++ yalla/Components/moleculus/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AnchorTag from "../atoms/Anchortag";
 import "../../app/globals.css";
 import { IoIosArrowDown } from "react-icons/io";
@@ -6,133 +6,100 @@ import { RxCross1 } from "react-icons/rx";
 import { IoMdMenu } from "react-icons/io";
 
 function Header() {
-  const open = function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.display = "block"
-    document.getElementById("mySidenav").style.opacity = "100%"
-    document.getElementById("mySidenav").style.right = "0"
-    document.getElementById("open").style.opacity = "0"
-    console.log("open is clicked");
+  const [isNavOpen, setNavOpen] = useState(false);
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
   };
 
-  const close = function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("mySidenav").style.display = "none"
-    document.getElementById("open").style.opacity = "100%"
-    document.getElementById("mySidenav").style.right = "-250px"
-
-    document.getElementById("mySidenav").style.opacity = "0%"
-
-
-    console.log("close is clicked");
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
   };
 
   return (
     <div>
       <div className="full">
-        <ul className=" gap-[50px] xl:flex xl:block xl:items-center xl:justify-center lg:block lg:items-center lg:justify-center lg:flex md:block md:flex md:items-center md:justify-center sm:hidden  hidden">
+        <ul className="gap-[50px] xl:flex xl:items-center xl:justify-center lg:flex lg:items-center lg:justify-center md:flex md:items-center md:justify-center sm:hidden hidden">
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/">
-              Home
-            </AnchorTag>
+            <AnchorTag variant="Header" href="/">Home</AnchorTag>
           </li>
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/about">
-              About
-            </AnchorTag>
+            <AnchorTag variant="Header" href="/about">About</AnchorTag>
           </li>
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/service">
+            <AnchorTag variant="Header" href="#" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
               Services <IoIosArrowDown />
-
             </AnchorTag>
-            <ul className='dropdown  absolute bg-white opacity-5 z-[9999] invisible py-[10px] pr-[50px] pl-[30px]'>
-              <li><AnchorTag variant='Headerh' href='/acservice'> AC Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/service'>over Servcies</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/tileservice'>Tiles Fixing</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/electricservice'>Electrician Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/plumbing'>Handyman & Plumber</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='bathroom'>Bathroom Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/painting'>Painting Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/kitchen'>Kitchen Service</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/celling'>Gypsum Ceiling</AnchorTag></li>
-            </ul>
+            {isDropdownVisible && (
+              <ul className="dropdown absolute bg-white z-[9999] visible py-[10px] pr-[50px] pl-[30px]">
+                <li><AnchorTag variant="Headerh" href="/acservice">AC Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/service">Other Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/tileservice">Tiles Fixing</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/electricservice">Electrician Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/plumbing">Handyman & Plumber</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/bathroom">Bathroom Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/painting">Painting Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/kitchen">Kitchen Service</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/celling">Gypsum Ceiling</AnchorTag></li>
+              </ul>
+            )}
           </li>
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/contactus">
-              Contact
-            </AnchorTag>
+            <AnchorTag variant="Header" href="/contactus">Contact</AnchorTag>
           </li>
         </ul>
 
         <span
           id="open"
-          className="block shadow-2xl cursor-pointer xl:hidden lg:hidden md:hidden sm:block "
+          className={`block shadow-2xl cursor-pointer xl:hidden lg:hidden md:hidden sm:block hidden`}
         >
-          <button onClick={open} >
+          <button  onClick={toggleNav}>
             <IoMdMenu fontSize={30} />
           </button>
         </span>
       </div>
 
-      
       <div
         id="mySidenav"
-        className="sidenav none absolute right-[-250px] h-[100vh] transition 0.5s ease-in bg-white z-[100000]"
+        className={`sidenav absolute w-[250px] h-[100vh] transition-transform 0.5s ease-ritht ease-out bg-white z-[100000] xl:hidden lg:hidden md:hidden  ${isNavOpen ? "block" : "hidden"} ${isNavOpen ? 'right-0' : 'right-[-250px]'} `}
       >
-        <button onClick={close} className="hover:rotate-[180] transition 0.5s ">
-          <a href="javascript:void(0)" class="closebtn">
-            <RxCross1 className=" text-black text-[30px]" />
-          </a>
+        <button onClick={toggleNav} className="hover:rotate-[180] transition">
+          <RxCross1 className="text-black text-[30px]" />
         </button>
-
 
         <ul className="flex flex-col items-center justify-center">
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/">
-              Home
-            </AnchorTag>
+            <AnchorTag variant="Header" href="/">Home</AnchorTag>
           </li>
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/about">
-              About
-            </AnchorTag>
+            <AnchorTag variant="Header" href="/about">About</AnchorTag>
           </li>
           <li>
-            {" "}
-            <AnchorTag variant="Header">
+            <AnchorTag variant="Header" href="#" onClick={toggleDropdown}>
               Services <IoIosArrowDown />
-
             </AnchorTag>
-            <ul className='dropdown  absolute bg-white opacity-5 z-[9999] invisible py-[10px] w-[100%] mr-[100px]'>
-              <li><AnchorTag variant='Headerh' href='/acservice'> AC Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/service'>over Servcies</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/tileservice'>Tiles Fixing</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/electricservice'>Electrician Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/plumbing'>Handyman & Plumber</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='bathroom'>Bathroom Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/painting'>Painting Services</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/kitchen'>Kitchen Service</AnchorTag></li>
-              <li><AnchorTag variant='Headerh' href='/celling'>Gypsum Ceiling</AnchorTag></li>
-            </ul>
+            {isDropdownVisible && (
+              <ul className="dropdown bg-white z-[9999] visible py-[10px] w-[100%]">
+                <li><AnchorTag variant="Headerh" href="/acservice">AC Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/service">Other Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/tileservice">Tiles Fixing</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/electricservice">Electrician Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/plumbing">Handyman & Plumber</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/bathroom">Bathroom Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/painting">Painting Services</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/kitchen">Kitchen Service</AnchorTag></li>
+                <li><AnchorTag variant="Headerh" href="/celling">Gypsum Ceiling</AnchorTag></li>
+              </ul>
+            )}
           </li>
           <li>
-            {" "}
-            <AnchorTag variant="Header" href="/contactus">
-              Contact
-            </AnchorTag>
+            <AnchorTag variant="Header" href="/contactus">Contact</AnchorTag>
           </li>
         </ul>
       </div>
     </div>
   );
-
 }
 
 export default Header;
